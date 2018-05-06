35261663f7eadde36adb8a1939cd67b1f269abbf
Chris Heckler <hecklerchris@hotmail.com>
2018-05-06 14:41:07 -0700

    var_conf_interval function added This function calculates a confidence interval for the variance

--- interval_estimation.py
+++ interval_estimation.py
@@ -19,9 +19,10 @@ def mean_conf_interval(data, conf):
         mean.
     
         Variables needed for computation are:
-        Data,n,mean,std,zscore,interval,lower
-        and upper bounds
+        Data,n,mean,std,zscore and interval. Lower
+        and upper bound confidence intervals are returned
     '''
+
     data = 1.0*np.array(data)
     n = data.shape[0]*data.shape[1]
     mean = np.array(data).mean()
@@ -30,5 +31,25 @@ def mean_conf_interval(data, conf):
     interval = zscore * std/np.sqrt(n)
     lower = mean-interval
     upper = mean+interval
+    
+    return lower,upper
+
+def var_conf_interval(data, conf):
+    ''' This function calculates a confidence interval for the 
+        variance.
+    
+        Variables needed for computation:
+        data,n,var, left and right chi statistic.
+        The upper and lower bound confidence intervals
+        returned.
+    '''
+
+    data = 1.0*np.array(data)
+    n = data.shape[0]*data.shape[1]
+    var = np.array(data).var(ddof=1)
+    chi_right_statistic = scs.chi2.isf(q=(1-conf)/2, df=209)
+    chi_left_statistic = scs.chi2.isf(q=conf+((1-conf)/2), df=209)
+    lower = (((n-1)*var)/chi_right_statistic)
+    upper = (((n-1)*var)/chi_left_statistic)
     
     return lower,upper
